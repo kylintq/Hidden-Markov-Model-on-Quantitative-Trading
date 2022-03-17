b9d3bffe5ea9d136214ae3500c17f7140c0040c6
kylintq <98428990+kylintq@users.noreply.github.com>
2022-03-17 19:34:51 -0400

    Update and rename 保存训练集策略.py to HMM_Trading_Strategy.py

--- HMM_Trading_Strategy.py
+++ HMM_Trading_Strategy.py
@@ -18,18 +18,18 @@ from time import *
 from pandas.plotting import register_matplotlib_converters
 from pandas.core.frame import DataFrame
 
-df=pd.read_csv('C:\\Users\\TQ\\Desktop\\实习\\处理后的数据.csv',
+df=pd.read_csv('C:\\Users\\TQ\\Desktop\\实习\\Data after Standardlization.csv',
                parse_dates=True)
 
-lnr=list(map(float,df['对数收益率']))
+lnr=list(map(float,df['ln_return']))
 
 alpha13=list(map(float,df['alpha13']))
 alpha34=list(map(float,df['alpha34']))
 alpha139=list(map(float,df['alpha139']))
 alpha150=list(map(float,df['alpha150']))
-vol=df['累计交易量']
+vol=df['Total_volume']
 vol=list(map(float,vol))
-updown=df['累计正负强度']
+updown=df['level_updown']
 updown=list(map(float,updown))
 
 A=np.column_stack([lnr,alpha13,alpha34,alpha139,alpha150,vol,updown])
@@ -43,17 +43,17 @@ print(states)
 
 
 
-df['类别']=states.tolist()
-classinformation=df['类别'].unique()
+df['type']=states.tolist()
+classinformation=df['type'].unique()
 
 for temp_classinformation in classinformation:
-    temp_data=df[df['类别'].isin([temp_classinformation])]
+    temp_data=df[df['type'].isin([temp_classinformation])]
     exec("df%s=temp_data"%temp_classinformation)
 
-dic={'df0':df0['对数收益率'].mean(),
-     'df1':df1['对数收益率'].mean(),
-     'df2':df2['对数收益率'].mean(),
-     'df3':df3['对数收益率'].mean(),}
+dic={'df0':df0['ln_return'].mean(),
+     'df1':df1['ln_return'].mean(),
+     'df2':df2['ln_return'].mean(),
+     'df3':df3['ln_return'].mean(),}
 d_order=sorted(dic.items(),key=lambda x:x[1],reverse=False) 
 max = sorted(dic, key=lambda x: dic[x],reverse=True)[0]
 max=int(max[2])
@@ -66,14 +66,14 @@ min2=sorted(dic, key=lambda x: dic[x],reverse=True)[2]
 max2=int(max2[2])
 min2=int(min2[2])
 '''
-writer=pd.ExcelWriter('C:\\Users\\TQ\\Desktop\\实习\\hmm结果.xlsx')
+writer=pd.ExcelWriter('C:\\Users\\TQ\\Desktop\\practice\\hmm.xlsx')
 df0.mean().to_excel(writer,)
 df1.mean().to_excel(writer,startcol=8)
 df2.mean().to_excel(writer,startrow=10)
 df3.mean().to_excel(writer,startcol=8,startrow=10)
 writer.save()'''
 
-df=pd.read_csv('C:\\Users\\TQ\\Desktop\\实习\\201601-202007.csv',
+df=pd.read_csv('C:\\Users\\TQ\\Desktop\\practice\\201601-202007.csv',
                parse_dates=True)
 close=df['close'].values
 date=pd.to_datetime(df['time'])
@@ -141,23 +141,21 @@ while i<len(close):
         i=i+1
 
 
-print('做多的总收益为：',np.sum(r_up))
-print('做多的次数为：',count_up)
-print('做多的平均持仓时间：',np.mean(time_up))
-print('做空的总收益为：',np.sum(r_down))
-print('做空的次数为：',count_down)
-print('做空的平均持仓时间：',np.mean(time_down))
+print('Total profit from buying long：',np.sum(r_up))
+print('Total count for buying long：',count_up)
+print('Average duration of buying long positions ：',np.mean(time_up))
+print('Total profit from selling short：',np.sum(r_down))
+print('Total count for selling short：',count_down)
+print('Average duration of selling short positions：',np.mean(time_down))
 
 '''
-data={'做多的总收益':np.sum(r_up),
-      '做多的平均收益':np.mean(r_up),
-      '做多的交易次数:':count_up,
-      '做多的平均持仓时间':np.mean(time_up),
-      '做空的总收益':np.sum(r_down),
-      '做空的平均收益':np.mean(r_down),
-      '做空的交易次数':count_down,
-      '做空的平均持仓时间':np.mean(time_down)}
-symbol=['策略']
+data={'Total profit from buying long':np.sum(r_up),
+      'Average profit from buying long':np.mean(r_up),
+      'Total count for buying long:':count_up,
+      'Average duration of buying long positions':np.mean(time_up),
+      'Total profit from selling short':np.sum(r_down),
+      'Average from selling short':np.mean(r_down),
+      'Total count for selling short':count_down,
+      'Average duration of selling short positions':np.mean(time_down)}
+symbol=['strategy']
 df_r=pd.DataFrame(data,index=symbol)
-df_r.to_excel('C:\\Users\\TQ\\Desktop\\实习\\测试集收益结果.xlsx',index=True,encoding='utf_8_sig')
-'''
